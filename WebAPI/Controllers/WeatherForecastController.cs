8df413b20afd2bf3ff67a9a2f99065062984219e
MetteThorn <metthorna@gmail.com>
2021-11-09 14:41:48 +0100

    Removed example in controller

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -26,13 +26,13 @@ namespace WebAPI.Controllers
         {
             _logger = logger;
             _weatherData = WeatherData.GetInstance();
-            _weatherData.AddWeatherForecast(new WeatherForecast()
-            {
-                Date = DateTime.Now,
-                Temperature = 69,
-                AirHumidity =1337,
-                AirPressure = 10
-            });
+            //_weatherData.AddWeatherForecast(new WeatherForecast()
+            //{
+            //    Date = DateTime.Now,
+            //    Temperature = 69,
+            //    AirHumidity =1337,
+            //    AirPressure = 10
+            //});
         }
         /// <summary>
         /// get the weather!
