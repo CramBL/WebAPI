3aa1ab0191be1c7defd26cc41fe5f9c14194a4c6
metthorna <90382045+metthorna@users.noreply.github.com>
2021-11-30 15:13:15 +0100

    Wrote 2 unittests for Weatherforecastcontroller

--- WebAPI.Tests/WeatherForecastControllerTests.cs
+++ WebAPI.Tests/WeatherForecastControllerTests.cs
@@ -7,12 +7,12 @@ using WebAPI.Data;
 using WebAPI.Models;
 using Xunit;
 using SQLitePCL;
+using System.Collections.Generic;
 
 namespace WebAPI.Tests
 {
     public class WeatherForecastControllerTests
     {
-        
         [Fact]
         public void canReturnLatestWeatherForecast()
         {
@@ -60,31 +60,66 @@ namespace WebAPI.Tests
             {
                 var WController = new WeatherForecastController(context);
                 var latest = WController.GetLatestAsync();
-                Assert.NotNull(latest);
-                //Assert.Equal(
+                var weatherForecastList = latest.Result.Value;
+                
+                //Assert.NotNull(latest);
+                Assert.Equal(3, weatherForecastList.Count);
             }
+        }
 
+        [Fact]
+        public void canReturnWeatherForecastAtDate()
+        {
+            var connection = new SqliteConnection("Data source = :memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
 
-
-
-
-
-
-
-
-
-            //var wfC = new WeatherForecastController();
-            //var w = new WeatherForecast {
-            //    WeatherForecastId = 12,
-            //    AirHumidity = 32,
-            //    AirPressure = 4,
-            //    Date = new DateTime(),
-            //    ObservedLocation = new ObservedLocation(),
-            //    ObservedLocationId = 354363,
-            //    Temperature = 666};
-
-
-            //Assert.Equal(w, w);
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.weatherForecasts.AddRange(
+                    new WeatherForecast{
+                        WeatherForecastId = 1,
+                        AirHumidity = 1,
+                        AirPressure = 1,
+                        Date = new DateTime(2001,01,01),
+                        ObservedLocation = new ObservedLocation(),
+                        ObservedLocationId = 1,
+                        Temperature = 1
+                    },
+                    new WeatherForecast{
+                        WeatherForecastId = 2,
+                        AirHumidity = 2,
+                        AirPressure = 2,
+                        Date = new DateTime(2002,02,02),
+                        ObservedLocation = new ObservedLocation(),
+                        ObservedLocationId = 2,
+                        Temperature = 2
+                    },
+                    new WeatherForecast{
+                        WeatherForecastId = 3,
+                        AirHumidity = 3,
+                        AirPressure = 3,
+                        Date = new DateTime(2003, 03, 03),
+                        ObservedLocation = new ObservedLocation(),
+                        ObservedLocationId = 3,
+                        Temperature = 3
+                    }
+                    );
+                context.SaveChanges();
+            }
+            using (var context = new ApplicationDbContext(options))
+            {
+                var WController = new WeatherForecastController(context);
+                DateTime testDate = new DateTime(2003,03,03);
+                var WeatherForecastsAtDate = WController.GetAtDateAsync(testDate);
+                var weatherForecastListAtDate = WeatherForecastsAtDate.Result.Value;
+                
+                Assert.Collection(weatherForecastListAtDate,
+                    forecast => Assert.Equal(testDate, forecast.Date));
+            }
         }
     }
 }
