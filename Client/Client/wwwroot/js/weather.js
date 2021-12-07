9043d50405c9d204688c84f0a37f948b01f405ee
metthorna <90382045+metthorna@users.noreply.github.com>
2021-12-07 14:13:18 +0100

    Made table to display data and styled it

---
+++ Client/Client/wwwroot/js/weather.js
@@ -0,0 +1,32 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/weatherHub").build();
+
+connection.on("ReceiveNewWeatherData", function (weatherData) {
+    var Date = weatherData.Date;
+    var Temperature = weatherData.Temperature;
+    var AirHumidity = weatherData.AirHumidity;
+    var AirPressure = weatherData.AirPressure;
+    var ObservedLocation = weatherData.ObservedLocation;
+    var LocationName = ObservedLocation.LocationName;
+    var Latitude = ObservedLocation.Latitude;
+    var Longitude = ObservedLocation.Longitude;
+
+    document.getElementById("date").innerHTML = Date;
+    document.getElementById("temperature").innerHTML = Temperature;
+    document.getElementById("airHumidity").innerHTML = AirHumidity;
+    document.getElementById("airPressure").innerHTML = AirPressure;
+    document.getElementById("locationName").innerHTML = LocationName;
+    document.getElementById("latitude").innerHTML = Latitude;
+    document.getElementById("longitude").innerHTML = Longitude;
+
+});
+
+
+
+
+//connection.on("ReceiveMessage", function (user, message) {
+//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//    var encodedMsg = user + " says " + msg;
+//    var li = document.createElement("li");
+//    li.textContent = encodedMsg;
+//    document.getElementById("messagesList").appendChild(li);
+//});
